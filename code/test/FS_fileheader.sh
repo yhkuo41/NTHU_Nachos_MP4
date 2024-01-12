5b3fb223308ce357b037a6d6e9810f9bff239391
Y.H. Kuo <yhkuo41@gmail.com>
2024-01-12 21:36:24 +0800

    MP4 Build

--- code/test/FS_fileheader.sh
+++ code/test/FS_fileheader.sh
@@ -2,6 +2,7 @@
 ../build.linux/nachos -mkdir /t0
 ../build.linux/nachos -mkdir /t1
 ../build.linux/nachos -cp num_100.txt /t0/f1
+../build.linux/nachos -cp num_1000.txt /t0/f2
 ../build.linux/nachos -mkdir /t0/aa
 ../build.linux/nachos -cp num_100.txt /t0/aa/f1
 ../build.linux/nachos -cp 64MB.txt /t0/aa/64MB
